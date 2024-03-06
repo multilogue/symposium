0aa6d11eea91f62263aebe0b71d0cc1dd0fabbda
Alex Fedotov <alxfed@gmail.com>
2024-03-05 18:50:36 -0600

    tests

--- src/tests/test_anthropic_rest.py
+++ src/tests/test_anthropic_rest.py
@@ -10,26 +10,9 @@ from os import environ
 from symposium.connectors import anthropic_rest as ant
 
 
-api_key             = environ.get("ANTHROPIC_API_KEY")
-organization        = environ.get("ANTHROPIC_ORGANIZATION", "")
-api_base            = environ.get("ANTHROPIC_API_BASE", "https://api.anthropic.com/v1")
-api_type            = environ.get("ANTHROPIC_VERSION", "2023-06-01")
-default_model       = environ.get("ANTHROPIC_DEFAULT_MODEL", "claude-instant-1.2")
-completion_model    = environ.get("ANTHROPIC_COMPLETION_MODEL",'claude-instant-1.2')
-message_model       = environ.get("ANTHROPIC_MESSAGE_MODEL",'claude-2.0')
-
-HUMAN_PREFIX        = "\n\nHuman:"
-MACHINE_PREFIX      = "\n\nAssistant:"
-
-headers = {
-    "x-api-key": api_key,
-    "anthropic-version": "2023-06-01",
-    "content-type": "application/json"
-}
-
-
 class TestAnthropicRest:
     def test_claud_message(self):
+        api_key = environ.get("ANTHROPIC_API_KEY")
         headers = {
             "x-api-key": api_key,
             "anthropic-version": "2023-06-01",
